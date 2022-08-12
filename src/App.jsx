86fc56c502ecdbc4d7ee1972534b62a6a738ed75
matuakira <108610776+matuakira@users.noreply.github.com>
2022-08-12 20:52:40 +0900

    update

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "ああああ",
+    "いいいいい"
+  ]);
+
+  const [completeTodos, setCompleteTodos] = useState([
+    "ううううう",
+    "うっほい"
+  ]);
+
+  /*
+    JSX記法でレンダリング。
+    map等でループするときは、仮想DOMの要素を一位に識別できるようにkeyが必要。
+
+   */
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="Todoを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
